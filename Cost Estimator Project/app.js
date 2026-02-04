1a93105e61dcac1a6df42c02db07b9aa23f439d3
Divyanshu951 <divyanshuk9515@gmail.com>
2026-02-04 15:17:24 +0530

    Add slider functionality and session card styling to Cost Estimator project

--- Cost Estimator Project/app.js
+++ Cost Estimator Project/app.js
@@ -1,12 +1,21 @@
 const tabs = document.querySelector(".tabs");
+const slider = document.querySelector(".slider");
 
 tabs.addEventListener("click", function (e) {
-  if (e.target.className !== "tab") return;
+  const ele = e.target;
+  if (ele.className !== "tab") return;
 
   const tabArray = Array.from(document.querySelectorAll(".tab"));
 
   // Remove active from all arrays
   tabArray.forEach((tab) => tab.classList.remove("active"));
+  ele.classList.add("active");
+});
 
-  e.target.classList.add("active");
+slider.addEventListener("change", function (e) {
+  const number = document.querySelector(".number");
+
+  number.textContent = new Intl.NumberFormat("en-US").format(
+    e.target.value * 10000,
+  );
 });
